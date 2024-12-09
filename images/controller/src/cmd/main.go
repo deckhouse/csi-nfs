867a0dcead61a56e5a900fe2c7ce8ba58c0eb27c
Pavel Karpov <pavel.karpov@flant.com>
2024-12-09 17:38:36 +0300

    fix: NFSStorageClass validation has been removed
    Signed-off-by: Pavel Karpov <pavel.karpov@flant.com>

--- images/controller/src/cmd/main.go
+++ images/controller/src/cmd/main.go
@@ -30,8 +30,6 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 
-	mc "d8-controller/api"
-
 	v1 "k8s.io/api/core/v1"
 	sv1 "k8s.io/api/storage/v1"
 	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -49,7 +47,6 @@ var (
 		extv1.AddToScheme,
 		v1.AddToScheme,
 		sv1.AddToScheme,
-		mc.AddToScheme,
 	}
 )
 
