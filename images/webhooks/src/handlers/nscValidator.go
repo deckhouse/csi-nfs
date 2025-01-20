0cf9c7081514e0ab6958a10636f943799685c0ca
v.oleynikov <vasily.oleynikov@flant.com>
2025-01-20 17:37:00 +0300

    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/webhooks/src/handlers/nscValidator.go
+++ images/webhooks/src/handlers/nscValidator.go
@@ -74,7 +74,7 @@ func NSCValidate(ctx context.Context, arReview *model.AdmissionReview, obj metav
 				"settings": map[string]interface{}{
 					"v3support": true,
 				},
-				"version": "1",
+				"version": 1,
 			},
 		})
 		if err != nil {
@@ -92,7 +92,7 @@ func NSCValidate(ctx context.Context, arReview *model.AdmissionReview, obj metav
 				"settings": map[string]interface{}{
 					"v3support": false,
 				},
-				"version": "1",
+				"version": 1,
 			},
 		})
 		if err != nil {
