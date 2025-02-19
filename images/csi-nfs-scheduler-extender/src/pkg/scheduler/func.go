3bfb9c8ea93cf01ed676da137aeaac1c7fa072e7
Slava V <vyacheslav.voytenok@flant.com>
2025-02-19 15:41:26 +0700

    [scheduler] run gci
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- images/csi-nfs-scheduler-extender/src/pkg/scheduler/func.go
+++ images/csi-nfs-scheduler-extender/src/pkg/scheduler/func.go
@@ -20,13 +20,14 @@ import (
 	"context"
 	"fmt"
 
-	"csi-nfs-scheduler-extender/pkg/logger"
 	v1alpha1 "github.com/deckhouse/csi-nfs/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"csi-nfs-scheduler-extender/pkg/logger"
 )
 
 const (
