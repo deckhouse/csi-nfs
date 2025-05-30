eb2eee6fd4213bf1982b66e0678183df1324b0a3
Nikolay Demchuk <nikolay.demchuk@flant.com>
2025-05-30 20:35:29 +1000

    fix Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>

--- hooks/go/consts/consts.go
+++ hooks/go/consts/consts.go
@@ -17,10 +17,10 @@ limitations under the License.
 package consts
 
 const (
-	ModuleName      	string = "csiNfs"
-	ModuleNamespace 	string = "d8-csi-nfs"
-	ModulePluralName 	string = "csi-nfs"
-	WebhookCertCn   	string = "webhooks"
+	ModuleName       string = "csi-nfs"
+	ModuleNamespace  string = "d8-csi-nfs"
+	ModulePluralName string = "csi-nfs"
+	WebhookCertCn    string = "webhooks"
 )
 
 var AllowedProvisioners = []string{
