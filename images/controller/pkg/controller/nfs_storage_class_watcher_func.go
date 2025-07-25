eee6e7e207ce2d2e8a4aed4942d19dfc0f64037e
v.oleynikov <vasily.oleynikov@flant.com>
2025-07-25 08:10:03 +0300

    [internal] add volumesnapshot controller mount options and storage classes annotations
    Signed-off-by: v.oleynikov <vasily.oleynikov@flant.com>

--- images/controller/pkg/controller/nfs_storage_class_watcher_func.go
+++ images/controller/pkg/controller/nfs_storage_class_watcher_func.go
@@ -678,7 +678,6 @@ func configureSecret(nsc *v1alpha1.NFSStorageClass, controllerNamespace string) 
 		},
 		StringData: map[string]string{
 			MountOptionsSecretKey: strings.Join(mountOptions, ","),
-
 		},
 	}
 
